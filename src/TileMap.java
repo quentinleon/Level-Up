5010d9260e3f39aa238bea923451ca5cc2537e11
Quentin Leon <qwolf1999@gmail.com>
2017-05-12 11:02:51 -0700

    Added Composite Shadows
    Shadows are now composite polygons. each tile can generate 2 shadow
    polys if the tile next to it is empty to save resources.

--- src/TileMap.java
+++ src/TileMap.java
@@ -5,22 +5,22 @@ import java.awt.image.BufferedImage;
 
 public class TileMap {
 	private Tile[][] map;
-	private Color shadow = new Color(0,0,0,100);
+	private Color shadow = new Color(0,0,0,120);
 	private Color tempWallColor = new Color(150,150,150);
+	
+	private boolean drawHeight = true;
+	private boolean shadows = true;
 	
 	public TileMap(int rows, int cols){
 		map = new Tile[rows][cols];
 		
 		for(int y = 0; y < map[0].length; y++){
 			for(int x = 0; x < map.length; x++){
-				if(y == 3){
-					map[x][y] = new Tile(TileType.none, 0);
-				}
-				if(y == 4){
-						map[x][y] = new Tile(TileType.stoneWall, 1.7);
+				if(y == 15 && x % 4 == 2){
+						map[x][y] = new Tile(TileType.stone, 1.7);
 				} else {
 					double height = 0;
-					TileType type = TileType.getRandomTile();
+					TileType type = TileType.sand;
 					map[x][y] = new Tile(type,height);
 				}
 			}
@@ -43,7 +43,7 @@ public class TileMap {
 		for(int y = yRange[0]; y < yRange[1]; y++){
 			for(int x = xRange[0]; x < xRange[1]; x++){
 				//if tile is on base level (0)
-				if(map[x][y].getHeight() == 0){
+				if(map[x][y].getHeight() == 0 || !drawHeight){
 					//get the tile's image
 					BufferedImage img = map[x][y].getImage();
 					if(img != null){
@@ -58,8 +58,12 @@ public class TileMap {
 			}
 		}
 		
+		//draw mobs
+		//draw objects
+		
 		//draw shadows 
 		for(int y = yRange[0]; y < yRange[1]; y++){
+			if(!shadows){break;};
 			for(int x = xRange[0]; x < xRange[1]; x++){
 				//set draw color to transparent black
 				g.setColor(shadow);
@@ -69,17 +73,31 @@ public class TileMap {
 					//draw the shadow
 					int xPos = (int)((x - xOffset) * Game.UNIT);
 					int yPos = (int)((y - yOffset) * Game.UNIT);
+					
 					//TODO make polygons based on variables, not constants
-					int[] xPoints = {xPos, xPos + (height), xPos + Game.UNIT + (height), xPos + Game.UNIT, xPos};
-					int[] yPoints = {yPos, yPos - (int)(height*1.5), yPos - (int)(height*1.5), yPos, yPos};
-					Polygon poly = new Polygon(xPoints, yPoints, xPoints.length);
-					g.fillPolygon(poly);
+					
+					//draw topShadow
+					if((y - 1) >= yRange[0] && map[x][y-1].getHeight() == 0){
+						int[] xPoints = {xPos, xPos + (height), xPos + Game.UNIT + (height), xPos + Game.UNIT, xPos};
+						int[] yPoints = {yPos, yPos - (int)(height*1.5), yPos - (int)(height*1.5), yPos, yPos};
+						Polygon poly = new Polygon(xPoints, yPoints, xPoints.length);
+						g.fillPolygon(poly);
+					}
+
+					//draw sideShadow
+					if((x + 1) < xRange[1] && map[x + 1][y].getHeight() == 0){
+						int[] xPoints2 = {xPos + Game.UNIT, xPos + height + Game.UNIT, xPos + height + Game.UNIT, xPos + Game.UNIT, xPos + Game.UNIT};
+						int[] yPoints2 = {yPos, yPos - (int)(height*1.5), yPos - (int)(height*1.5) + Game.UNIT, yPos + Game.UNIT, yPos};
+						Polygon sideShadow = new Polygon(xPoints2, yPoints2, xPoints2.length);
+						g.fillPolygon(sideShadow);
+					}
 				}
 			}
 		}
 		
 		//draw walls and top levels
 		for(int y = yRange[0]; y < yRange[1]; y++){
+			if(!drawHeight){break;};
 			for(int x = xRange[0]; x < xRange[1]; x++){
 				//set draw color to transparent black
 				g.setColor(tempWallColor);
@@ -99,5 +117,8 @@ public class TileMap {
 				}
 			}
 		}
+		
+		//draw lights?
+		
 	}
 }
