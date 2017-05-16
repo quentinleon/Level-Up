a17016518c575cbe5070b50e7bd25283f7ef358f
sophiemortaz <sophie.mortaz@gmail.com>
2017-05-16 09:49:11 -0700

    Draw Player

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,30 @@
 import java.awt.Graphics;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Player implements Mob {
 
 	public InputHandler input;
+	
 	private double xPos = 0;
 	private double yPos = 0;
 	private double speed = 5;
 	
+	BufferedImage right;
+	
 	public Player(Game game){
 		input = new InputHandler(game);
+		
+		right = null;
+		try{
+			right = ImageIO.read(new File("resources/mobs/player/right.png"));
+		} catch(IOException e){
+			System.out.println("Could not load all player sprites!");
+		}
 	}
 	
 	public void init() {
@@ -42,10 +58,13 @@ public class Player implements Mob {
 		return yPos;
 	}
 
-	@Override
 	public void draw(Graphics g) {
-		// TODO Auto-generated method stub
-		
+		if(right != null){
+			int drawPosX = (int)((0) * Game.UNIT);
+			int drawPosY = (int)((0) * Game.UNIT);
+			g.drawImage(right, drawPosX, drawPosY, drawPosX + Game.UNIT, drawPosY + Game.UNIT,
+					0, 0, right.getHeight(null), right.getHeight(null), null);
+		}
 	}
 
 }
