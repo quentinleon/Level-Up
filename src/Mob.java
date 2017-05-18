c610a756c2c660c1d8849c81aba207b2506fe0e7
Quentin Leon <qwolf1999@gmail.com>
2017-05-18 08:15:22 -0700

    Fixed Tile Tearing
    Small fix to drawMap() to remove tile tearing.

--- src/Mob.java
+++ src/Mob.java
@@ -3,7 +3,7 @@ import java.awt.Graphics;
 public interface Mob {
 	public void init(); //called once at game start
 	public void update(); //called 60 times a second
-	public void draw(Graphics g);
+	public void draw(Graphics g); 
 	
 	public double getX();
 	public double getY();
