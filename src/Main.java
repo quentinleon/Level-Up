58da335aeb0fe61dfc4d3d4158c279cec4567674
Quentin Leon <qwolf1999@gmail.com>
2017-05-22 12:36:19 -0700

    Added an Animation subclass to Player
    This is to let us quickly add variable fps animations to the player.

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
 	public static void main(String args[]){
 		Game game = new Game();
 		JFrame frame = new JFrame("Level-Up"); //Create a new window named "Level-Up"
-		frame.setSize(new Dimension(800,600)); //Set it's dimension
-		frame.setLocation(new Point(200,200)); //Set it's position
+		frame.setSize(new Dimension(1920,1080)); //Set it's dimension
+		frame.setLocation(new Point(0,0)); //Set it's position
 		frame.setContentPane(game.renderer); //Make the game the frame content
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Enable the close button
 		frame.setVisible(true); //Show the frame
